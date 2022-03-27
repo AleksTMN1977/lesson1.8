1b7de667c5196b286697d3d575cf743fe57499d3
AleksTMN <kirilloff_77@mail.ru>
2022-03-27 12:25:49 +0500

    method

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,34 @@
 package ru.skypro;
 
+import java.time.LocalDate;
+
 public class Main {
+    public static void checkYear(int year) {
+        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год является невисокосным");
+        }
+    }
+
+    public static void versionOs(int currentYear, int clientOS) {
+        if (clientOS == 0 && currentYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && currentYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS != 0 && currentYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        int year = 2022;
+        checkYear(year);
+        int currentYear = LocalDate.now().getYear();
+        int clientOs = 1;
+        versionOs(currentYear, clientOs);
     }
+
 }
